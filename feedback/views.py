98d21b2de596bc0073848b0b2d22a9c1b761b895
minasedr <miansedrg@gmail.com>
2024-03-15 21:47:21 +0300

    implement feedback app

---
+++ feedback/views.py
@@ -0,0 +1,24 @@
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .serializers import FeedbackSerializer
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def rate(request):
+    serializer = FeedbackSerializer(request.data)
+
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK,
+        )
+
+    return Response(
+        serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST,
+    )
--- travelwave/urls.py
+++ travelwave/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("api/accounts/", include("accounts.urls")),
     path("api/vehicles/", include("vehicles.urls")),
     path("api/rides/", include("rides.urls")),
+    path("api/feedback/", include("feedback.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
