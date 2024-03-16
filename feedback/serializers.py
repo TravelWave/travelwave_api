e8a7254b4a9fc979b8e049981fb6da721a430ee3
minasedr <miansedrg@gmail.com>
2024-03-16 14:26:12 +0300

    backend(feat): implement chat module

--- feedback/serializers.py
+++ feedback/serializers.py
@@ -26,13 +26,5 @@ class FeedbackSerializer(serializers.ModelSerializer):
             "passenger",
             "driver",
             "rating",
-            "sos_notification",
             "feedback",
         ]
-
-    def create(self, validated_data):
-        if validated_data.get("sos_notification"):
-            # TODO: send notification to the admin of the system
-            pass
-
-        return super().create(**validated_data)
--- travelwave/urls.py
+++ travelwave/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("api/vehicles/", include("vehicles.urls")),
     path("api/rides/", include("rides.urls")),
     path("api/feedback/", include("feedback.urls")),
+    path("api/chats/", include("chats.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
